ea0fc699e135ad1bf1ab497fff051d0c12168ec3
malochiftikhar <iftikhar.khan@academytrainee.com>
2018-10-09 14:23:38 +0100

    String task done

---
+++ src/main/scala/Strings.scala
@@ -0,0 +1,11 @@
+object Strings {
+  def main(args: Array[String]): Unit = {
+
+    println(twoPara("Hello", 3))
+  }
+
+  def twoPara(text : String , number : Int ) = {
+
+    text.takeRight(number)
+  }
+}
