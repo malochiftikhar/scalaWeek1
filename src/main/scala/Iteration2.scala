9a02cfd7692e1a96f695ce040c0fe5949633a67c
malochiftikhar <iftikhar.khan@academytrainee.com>
2018-10-09 15:35:58 +0100

    Iteration 2 completed

---
+++ src/main/scala/Iteration2.scala
@@ -0,0 +1,17 @@
+object Iteration2 {
+
+  def main(args: Array[String]): Unit = {
+
+    rectangle("H", 4)
+  }
+
+  def rectangle(text : String, number : Int): Unit ={
+
+    for (i<- 0 until number){
+    for (i<-0 until number) {
+      print(text)
+    }
+      println()
+    }
+  }
+}
